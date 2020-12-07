c8cca6613cdae99dc85e6a2e0201800a499b996a
ariju <70578567+ariju@users.noreply.github.com>
2020-12-07 10:22:35 +0900

    spinとstopのclickイベント

--- js/main.js
+++ js/main.js
@@ -9,20 +9,32 @@
 // imgとstopは他のクラスでも使用するからthis
       const section = document.createElement('section');
       section.classList.add('panel');
+
 // img要素を作ってsrc属性にimg/seven.pngとする
       this.img = document.createElement('img');
-      this.img.src = 'img/seven.png';
+// リロードする度に画像が変わる
+      this.img.src = this.getRandomImage();
+// timeoutIdプロパティを初期化最初は値が定まってないからundefined
+      this.timeoutId = undefined;
+
 // div要素を作ってstopを表示stopクラスにstopを追加する
       this.stop = document.createElement('div');
       this.stop.textContent = 'STOP';
       this.stop.classList.add('stop');
+// stopボタンをクリックした時の処理
+      this.stop.addEventListener('click', () => {
+        clearTimeout(this.timeoutId);
+      });
+
 // imgとstopをsectionの子要素として追加
       section.appendChild(this.img);
       section.appendChild(this.stop);
+
 // sectionをmainに追加
       const main = document.querySelector('main');
       main.appendChild(section);
     }
+
 // getRandomImageメソッドにimagesを定義して
 // その中に配列として画像を入れてランダムに選んだ要素を返す
     getRandomImage() {
@@ -33,10 +45,16 @@
       ];
       return images[Math.floor(Math.random() * images.length)];
     }
+
 // spinメソッドの定義。画像を設定
 // getRandomImageメソッドにまとめる
     spin() {
       this.img.src = this.getRandomImage();
+// setTimeoutを使って50ミリ秒後にspinメソッドを繰り返す      
+// timeoutIdプロパティをsetTimeoutの返り値にする
+      this.timeoutId = setTimeout(() => {
+        this.spin();
+      }, 50);
     }
   }
 
@@ -46,6 +64,7 @@
     new Panel(),
     new Panel(),
   ];
+
 // spinをクリックした時の処理
   const spin = document.getElementById('spin');
   spin.addEventListener('click', () => {
